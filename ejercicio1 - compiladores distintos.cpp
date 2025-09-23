0d95b91546b94af71a39e57976431072c880b3af
Adrian <adrianrw@msmk.university>
2025-09-23 16:30:09 +0200

    ejercicio 1 - compiladores distintos

---
+++ ejercicio1 - compiladores distintos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int Add(int x, int y)
+{
+    std::cout << "En Add(), recibido" << x << "y" << y << "\n";
+    return (x + y);
+}
+
+int main()
+{
+    std::cout << "En main()\n";
+    std::cout << "\nllamada a Add ()\n";
+    std::cout << "El valor devuelto es: " << Add(3, 4);
+    std::cout << "\nvuelta a main ().\n";
+    std::cout << "\nsalida de main().\n";
+    return 0;
+}
